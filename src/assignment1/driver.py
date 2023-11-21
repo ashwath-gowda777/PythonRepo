6b75aaf973ab5d943438a62c847127155a2d07d6
Ashwath-gowda777 <ashwathgowda216@gmail.com>
2023-11-21 17:39:08 +0530

    list problem on commands - Q1

--- src/assignment1/driver.py
+++ src/assignment1/driver.py
@@ -2,4 +2,4 @@ from util import list_operations
 
 N = int(input("Enter the number of commands: "))
 user_commands = [input().split() for i in range(N)]
-list_operations(user_commands)src
+list_operations(user_commands)
