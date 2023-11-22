4f176ca1c535e35d07dbcf080ddd7851f6583613
Ashwath-gowda777 <ashwathgowda216@gmail.com>
2023-11-22 16:52:49 +0530

    formatted test case

---
+++ test/assignment6/test_Q6.py
@@ -0,0 +1,10 @@
+import unittest
+from src.assignment6.util import print_formatted
+
+class TestPrintFormatted(unittest.TestCase):
+    def test_print_formatted(self):
+        with self.assertRaises(SystemExit):
+            print_formatted(5)
+
+if __name__ == '__main__':
+    unittest.main()
