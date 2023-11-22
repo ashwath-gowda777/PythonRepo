6498faa1ca819fe238e59c84cf421b04d137c720
Ashwath-gowda777 <ashwathgowda216@gmail.com>
2023-11-22 14:50:50 +0530

    runner_up_score comleted unit test

---
+++ test/assignment3/test_Q3.py
@@ -0,0 +1,16 @@
+
+# test_Q3
+
+import unittest
+from src.assignment3.util import runner_up_score
+
+class TestRunnerUpScore(unittest.TestCase):
+    def test_runner_up_score(self):
+        # Example test case
+        values = [1, 2, 3, 4]
+        expected_result = 3  # The second-to-last element after sorting is 3
+        result = runner_up_score(values)
+        self.assertEqual(result, expected_result)
+
+if __name__ == '__main__':
+    unittest.main()
