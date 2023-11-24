f7cb00345ec535e1daa153ceec9d5762bbde4ea5
Ashwath Gowda G <125112241+ashwath-gowda777@users.noreply.github.com>
2023-11-24 10:48:19 +0530

    This is a driver file for Finding Calendar Module

--- src/assignment8/driver.py
+++ src/assignment8/driver.py
@@ -4,4 +4,5 @@ if __name__ == '__main__':
     month, day, year = map(int, input("Enter month, day, and year (separated by space): ").split())
     result = day_name(month, day, year)
     print("The day is: " + result)
+    
 
