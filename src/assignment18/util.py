f82776939a512e8c947ddc01782e610f238701e4
Ashwath Gowda G <125112241+ashwath-gowda777@users.noreply.github.com>
2023-11-24 12:03:59 +0530

    This is a utils file for Finding Iterables and Iterators

--- src/assignment18/util.py
+++ src/assignment18/util.py
@@ -5,5 +5,3 @@ def Iterables_and_Iterators_probability(letters, k, target_letter):
     contains = [word for word in tuples if target_letter in word]
     probability = len(contains) / len(tuples)
     return probability
-
-
