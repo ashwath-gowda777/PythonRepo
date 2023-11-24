aa2620b4b68e6b6646cf07aba4786735edd2b852
Ashwath Gowda G <125112241+ashwath-gowda777@users.noreply.github.com>
2023-11-24 10:30:02 +0530

    This is a driver file for Finding the Runner-Up Score!

--- src/assignment3/driver.py
+++ src/assignment3/driver.py
@@ -3,3 +3,4 @@ from src.assignment3.util import runner_up_score
 if __name__ == "__main__":
     N = int(input("Enter number of score need one line it will take it string space it : "))
     runner_up_score(N)   
+  
