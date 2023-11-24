1fec27abdfd4526b6c5a8f87ef7fff088667dfb0
Ashwath Gowda G <125112241+ashwath-gowda777@users.noreply.github.com>
2023-11-24 12:20:42 +0530

    This is a unittest file for Finding Calendar Module

--- test/assignment8/test_Q8.py
+++ test/assignment8/test_Q8.py
@@ -6,6 +6,5 @@ class TestGetDayName(unittest.TestCase):
     def test_Now_Testing_is_done(self):
         self.assertEqual(day_name(11, 23, 2023), "THURSDAY")
 
-
 if __name__ == '__main__':
     unittest.main()
